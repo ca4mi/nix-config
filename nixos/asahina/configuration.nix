62b25271ba68656309965e09d5f29f2e3b5ba39d
ca4mi <mail@ca4mi.net>
2025-06-24 11:34:02 +0000

    some boot tests

--- nixos/asahina/configuration.nix
+++ nixos/asahina/configuration.nix
@@ -16,6 +16,13 @@
   # Bootloader.
   # boot.loader.systemd-boot.enable = true;
   # boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.loader.grub = {
+    enable = true;
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+  };
+
   boot.zfs.forceImportRoot = true;
 
   networking.hostName = "asahina";
