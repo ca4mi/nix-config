33636d697dc4754278c2d8a3be6190e21ae51141
ca4mi <mail@ca4mi.net>
2025-07-02 22:19:31 +0800

    tailscale config

--- nixos/_common/tailscale/default.nix
+++ nixos/_common/tailscale/default.nix
@@ -10,30 +10,11 @@
 
   networking.firewall.allowedUDPPorts = [ config.services.tailscale.port ];
   networking.firewall.trustedInterfaces = [ "tailscale0" ];
+  networking.firewall.checkReversePath = "loose";
 
-  systemd.services.tailscale-autoconnect = {
-    description = "Automatic connection to Tailscale";
-
-    after = [ "network-pre.target" "tailscale.service" ];
-    wants = [ "network-pre.target" "tailscale.service" ];
-    wantedBy = [ "multi-user.target" ];
-
-    serviceConfig.Type = "oneshot";
-
-    # have the job run this shell script
-    script = with pkgs; ''
-      # wait for tailscaled to settle
-      sleep 2
-
-      # check if we are already authenticated to tailscale
-      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
-      if [ $status = "Running" ]; then # if so, then do nothing
-        exit 0
-      fi
-
-      # otherwise authenticate with tailscale
-      ${tailscale}/bin/tailscale up -authkey ${config.age.secrets.tailscaleAuthKey.path}
-    '';
+  services.tailscale = {
+    enable = true;
+    authKeyFile = config.age.secrets.tailscaleAuthKey.path;
   };
-
 }
+
