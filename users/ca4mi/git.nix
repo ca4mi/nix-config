acab1b1ff750a2ec14a1e0cdaa289c5470d958a4
ca4mi <mail@ca4mi.net>
2025-12-03 22:01:50 +0800

    git config changed

--- users/ca4mi/git.nix
+++ users/ca4mi/git.nix
@@ -5,15 +5,15 @@
   ...
 }:
 {
-
   programs.git = {
     enable = true;
-    userName = "ca4mi";
-    userEmail = "mail@ca4mi.net";
-
-    extraConfig = {
-      core = {
-        sshCommand = "ssh -o 'IdentitiesOnly=yes' -i ~/.ssh/ca4mi";
+    settings = {
+      user.name = "ca4mi";
+      user.email = "mail@ca4mi.net";
+      extraConfig = {
+        core = {
+          sshCommand = "ssh -o 'IdentitiesOnly=yes' -i ~/.ssh/ca4mi";
+	};
       };
     };
   };
