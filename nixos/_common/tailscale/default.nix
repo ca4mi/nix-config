2d5800a986099ea6d90ac2f373aa869f2c314a1b
ca4mi <mail@ca4mi.net>
2026-01-01 14:19:50 +0800

    ts changes

--- nixos/_common/tailscale/default.nix
+++ nixos/_common/tailscale/default.nix
@@ -7,9 +7,10 @@
 }:
 {
   networking.firewall = {
+    allowedUDPPorts = [ config.services.tailscale.port ];
     trustedInterfaces = [ "tailscale0" ];
+    checkReversePath = "loose";
   };
-
   services.tailscale = {
     enable = true;
     authKeyFile = config.age.secrets.tailscaleAuthKey.path;
