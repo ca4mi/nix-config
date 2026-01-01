0d7bb7a26f28f444567bd529189bdd67e94e395c
ca4mi <mail@ca4mi.net>
2026-01-01 13:07:55 +0800

    remove unstable package

--- nixos/_common/tailscale/default.nix
+++ nixos/_common/tailscale/default.nix
@@ -7,12 +7,10 @@
 }:
 {
   networking.firewall = {
-    allowedUDPPorts = [ config.services.tailscale.port ];
     trustedInterfaces = [ "tailscale0" ];
-    checkReversePath = "loose";
   };
+
   services.tailscale = {
-    package = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system}.tailscale;
     enable = true;
     authKeyFile = config.age.secrets.tailscaleAuthKey.path;
   };
