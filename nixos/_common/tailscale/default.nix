aaf9d4ccd1ebd7ac4a4dbe48954def9278d7cbad
ca4mi <mail@ca4mi.net>
2026-01-01 12:45:09 +0800

    authkey changed

--- nixos/_common/tailscale/default.nix
+++ nixos/_common/tailscale/default.nix
@@ -6,13 +6,11 @@
   ...
 }:
 {
-
   networking.firewall = {
     allowedUDPPorts = [ config.services.tailscale.port ];
     trustedInterfaces = [ "tailscale0" ];
     checkReversePath = "loose";
   };
-
   services.tailscale = {
     package = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system}.tailscale;
     enable = true;
