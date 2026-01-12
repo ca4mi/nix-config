f2544684526ada457faa13f9da7844b8b95839f6
ca4mi <mail@ca4mi.net>
2026-01-12 10:10:39 +0800

    flake updates

--- nixos/asahina/configuration.nix
+++ nixos/asahina/configuration.nix
@@ -184,6 +184,7 @@
 
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [
+    8096
   ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
