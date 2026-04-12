eecefb23c04c327428da46df2d780a51d502b5f3
ca4mi <mail@ca4mi.net>
2026-04-12 20:56:22 +0800

    daily updates

--- nixos/asahina/configuration.nix
+++ nixos/asahina/configuration.nix
@@ -76,7 +76,7 @@
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.production;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
   # prime 
